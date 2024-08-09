78be3e1f09767f4de3cfee6d768a7b2d7b24a944
Rauan Kaztaev <rtishtyk@gmail.com>
2024-08-09 20:56:20 +0500

    Кэширование достижении

---
+++ src/main/java/faang/school/achievement/cache/AchievementCache.java
@@ -0,0 +1,28 @@
+package faang.school.achievement.cache;
+
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class AchievementCache {
+    private final Map<String, Achievement> cachedAchievements = new HashMap<>();
+    private final AchievementRepository achievementRepository;
+
+    public Achievement getAchievement(String title) {
+        return cachedAchievements.get(title);
+    }
+
+    @PostConstruct
+    private void cacheAchievements() {
+        List<Achievement> achievements = achievementRepository.findAll();
+        achievements.forEach(achievement -> cachedAchievements.put(achievement.getTitle(), achievement));
+    }
+}
