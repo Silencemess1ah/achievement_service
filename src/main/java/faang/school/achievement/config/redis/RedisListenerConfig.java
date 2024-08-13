1ab5bfa38a4220c98294bd02eb7ec844d8416351
Arman Bistibekov <bistibekov.a@gmail.com>
2024-08-13 11:51:55 -0400

    Implemented RedisListenerConfigListenerConfig

---
+++ src/main/java/faang/school/achievement/config/redis/RedisListenerConfig.java
@@ -0,0 +1,46 @@
+package faang.school.achievement.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisListenerConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.host.channels.mentorship}")
+    private String mentorshipChannelName;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        System.out.println(port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public ChannelTopic mentorshipChannelTopic() {
+        return new ChannelTopic(mentorshipChannelName);
+    }
+    @Bean
+    public MessageListenerAdapter mentorshipListenerAdapter() {
+        return new MessageListenerAdapter(); // METNORSHIP LISTENER;
+    }
+    @Bean
+    public RedisMessageListenerContainer messageListenerContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(mentorshipListenerAdapter(), mentorshipChannelTopic());
+        return container;
+    }
+
+}
