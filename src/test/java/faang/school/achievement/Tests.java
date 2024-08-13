2e02ed25579243ba87256bfd1055c6b122043ccf
Rauan Kaztaev <rtishtyk@gmail.com>
2024-08-13 16:44:43 +0500

    Дописал тесты

--- src/test/java/faang/school/achievement/Tests.java
+++ src/test/java/faang/school/achievement/Tests.java
@@ -5,7 +5,9 @@ import faang.school.achievement.repository.AchievementRepository;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -14,6 +16,7 @@ import static org.mockito.Mockito.when;
 public class Tests {
 
     @Bean
+    @Profile("default")
     public AchievementRepository achievementRepository() {
         Achievement expected = new Achievement();
         expected.setTitle("Some");
@@ -21,6 +24,14 @@ public class Tests {
         // using Mockito is just an example
         AchievementRepository achievementRepository = Mockito.mock(AchievementRepository.class);
         when(achievementRepository.findAll()).thenReturn(expectedAchievements);
+        return achievementRepository;
+    }
+
+    @Bean
+    @Profile("empty")
+    public AchievementRepository achievementRepositoryWithoutData() {
+        AchievementRepository achievementRepository = Mockito.mock(AchievementRepository.class);
+        when(achievementRepository.findAll()).thenReturn(Collections.emptyList());
         return achievementRepository;
     }
 
