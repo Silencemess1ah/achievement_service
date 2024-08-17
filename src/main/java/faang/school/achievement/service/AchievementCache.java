ddbd2d6dc59abad1a5054cc8db670e9a6bba6341
sta1er <vlad.sokogubov@gmail.com>
2024-08-17 15:51:12 +0300

    Кэширование достижений

--- src/main/java/faang/school/achievement/service/AchievementCache.java
+++ src/main/java/faang/school/achievement/service/AchievementCache.java
@@ -7,8 +7,8 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +19,7 @@ public class AchievementCache {
 
     @PostConstruct
     public void init() {
-        achievementCache = new HashMap<>();
+        achievementCache = new ConcurrentHashMap<>();
         achievementRepository.findAll().forEach(achievement ->
                 achievementCache.put(achievement.getTitle(), achievement));
     }
