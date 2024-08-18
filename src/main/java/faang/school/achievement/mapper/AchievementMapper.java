8f1c1bfb0eafe03a78c83ce80ebde3c5e82491dc
Grisha Tkach <grisatkac@mail.ru>
2024-08-19 00:04:20 +0300

    add mappers

---
+++ src/main/java/faang/school/achievement/mapper/AchievementMapper.java
@@ -0,0 +1,16 @@
+package faang.school.achievement.mapper;
+
+import faang.school.achievement.dto.AchievementDto;
+import faang.school.achievement.model.Achievement;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "Spring")
+public interface AchievementMapper {
+
+
+    AchievementDto toDto(Achievement achievement);
+
+    List<AchievementDto> toDtoList(List<Achievement> list);
+}
