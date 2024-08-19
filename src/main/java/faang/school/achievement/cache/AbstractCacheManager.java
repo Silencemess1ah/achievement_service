cd9d09947accb7160f3adb2e8f71cb892d1cb3f4
malkov34 <malk.2011@yandex.ru>
2024-08-19 19:38:18 +0300

    BJS2-16056: AchievementCache

---
+++ src/main/java/faang/school/achievement/cache/AbstractCacheManager.java
@@ -0,0 +1,27 @@
+package faang.school.achievement.cache;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author Evgenii Malkov
+ */
+@Component
+@RequiredArgsConstructor
+public abstract class AbstractCacheManager<T> {
+
+    protected final ObjectMapper mapper;
+    protected final RedisTemplate<String, Object> redisTemplate;
+
+    protected void put(String key, Map<String, T> values) {
+        this.redisTemplate.opsForHash().putAll(key, values);
+    }
+
+    protected T get(String key, String hashKey) {
+        return (T) redisTemplate.opsForHash().get(key, hashKey);
+    }
+}
