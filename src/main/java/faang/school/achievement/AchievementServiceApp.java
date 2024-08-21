d01d5f947631c4c5a127f5c4bba864fc36706728
Grisha Tkach <grisatkac@mail.ru>
2024-08-21 19:17:02 +0300

    move object mapper configuration

--- src/main/java/faang/school/achievement/AchievementServiceApp.java
+++ src/main/java/faang/school/achievement/AchievementServiceApp.java
@@ -19,9 +19,4 @@ public class AchievementServiceApp {
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 }
