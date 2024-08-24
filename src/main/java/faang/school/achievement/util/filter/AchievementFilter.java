90778113a02f0d2d4d7bcdbef6ee223bd42e7a81
OlgaK888 <olgakurilovi4@gmail.com>
2024-08-24 18:41:53 +0300

    Added @Component

--- src/main/java/faang/school/achievement/util/filter/AchievementFilter.java
+++ src/main/java/faang/school/achievement/util/filter/AchievementFilter.java
@@ -2,9 +2,11 @@ package faang.school.achievement.util.filter;
 
 import faang.school.achievement.dto.AchievementFilterDto;
 import faang.school.achievement.model.Achievement;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
+@Component
 public interface AchievementFilter {
 
     boolean isApplicable(AchievementFilterDto filters);
