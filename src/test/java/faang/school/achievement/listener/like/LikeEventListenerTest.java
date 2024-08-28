d7a6744e98b723a5d3802b3f0172519621e67945
Rauan Kaztaev <rtishtyk@gmail.com>
2024-08-29 00:31:10 +0500

    Добавил поле в LikeEvent

--- src/test/java/faang/school/achievement/listener/like/LikeEventListenerTest.java
+++ src/test/java/faang/school/achievement/listener/like/LikeEventListenerTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.connection.Message;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,7 +45,7 @@ class LikeEventListenerTest {
 
     @BeforeEach
     void setUp() {
-        likeEvent = new LikeEvent(UUID.randomUUID(),1, 1, 1);
+        likeEvent = new LikeEvent(UUID.randomUUID(),1, 1, 1, LocalDateTime.now());
     }
 
     @Test
