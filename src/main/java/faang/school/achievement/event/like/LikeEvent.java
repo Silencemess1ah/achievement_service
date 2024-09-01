94cf82f7da46a07c0bdbb84ccca8a7fdb7286cae
Rauan Kaztaev <rtishtyk@gmail.com>
2024-09-02 01:15:28 +0500

    Убрал лишнее поле в LikeEvent

--- src/main/java/faang/school/achievement/event/like/LikeEvent.java
+++ src/main/java/faang/school/achievement/event/like/LikeEvent.java
@@ -1,24 +1,24 @@
 package faang.school.achievement.event.like;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import faang.school.achievement.event.Event;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LikeEvent implements Event {
     private UUID eventId;
     private long authorId;
     private long postId;
     private long likeId;
-    private LocalDateTime timeStamp;
 
     @Override
     public long getUserId() {
--- src/test/java/faang/school/achievement/listener/like/LikeEventListenerTest.java
+++ src/test/java/faang/school/achievement/listener/like/LikeEventListenerTest.java
@@ -14,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.connection.Message;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -45,7 +44,7 @@ class LikeEventListenerTest {
 
     @BeforeEach
     void setUp() {
-        likeEvent = new LikeEvent(UUID.randomUUID(),1, 1, 1, LocalDateTime.now());
+        likeEvent = new LikeEvent(UUID.randomUUID(),1, 1, 1);
     }
 
     @Test
