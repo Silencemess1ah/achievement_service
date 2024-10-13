e3b7a348846b8eb025f0ad7346788ce2893a5dfc
ifef228 <ifef228@gmail.com>
2024-10-13 08:55:57 +0300

    AchievementServiceImplTest

---
+++ src/test/java/faang/school/achievement/service/AchievementServiceImplTest.java
@@ -0,0 +1,101 @@
+package faang.school.achievement.service;
+
+import faang.school.achievement.mapper.AchievementProgressMapper;
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.model.AchievementProgress;
+import faang.school.achievement.model.UserAchievement;
+import faang.school.achievement.repository.AchievementProgressRepository;
+import faang.school.achievement.repository.UserAchievementRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class AchievementServiceImplTest {
+    @InjectMocks
+    private AchievementServiceImpl service;
+
+    @Mock
+    private UserAchievementRepository userAchievementRepository;
+
+    @Mock
+    private AchievementProgressRepository achievementProgressRepository;
+
+    @Mock
+    private AchievementProgressMapper achievementProgressMapper;
+
+    private long userId;
+    private long achievementId;
+    private AchievementProgress achievementProgress;
+    private Achievement achievement;
+
+    @BeforeEach
+    public void init() {
+        userId = 1L;
+        achievementId = 1L;
+        achievementProgress = new AchievementProgress();
+        achievement = new Achievement();
+    }
+
+    @Test
+    void hasAchievement_whenOk() {
+        service.hasAchievement(userId, achievementId);
+
+        Mockito.verify(userAchievementRepository, Mockito.times(1))
+                .existsByUserIdAndAchievementId(userId, achievementId);
+    }
+
+    @Test
+    void createProgressIfNecessary_whenOk() {
+        service.createProgressIfNecessary(userId, achievementId);
+
+        Mockito.verify(achievementProgressRepository, Mockito.times(1))
+                .createProgressIfNecessary(userId, achievementId);
+    }
+
+    @Test
+    void getProgress_whenOk() {
+        Mockito.when(achievementProgressRepository.findByUserIdAndAchievementId(userId, achievementId))
+                        .thenReturn(Optional.ofNullable(achievementProgress));
+
+        service.getProgress(userId, achievementId);
+
+        Mockito.verify(achievementProgressRepository, Mockito.times(1))
+                .findByUserIdAndAchievementId(userId, achievementId);
+        Mockito.verify(achievementProgressMapper, Mockito.times(1))
+                .toDto(achievementProgress);
+    }
+
+    @Test
+    void getProgress_whenProgressNotExist() {
+        Mockito.when(achievementProgressRepository.findByUserIdAndAchievementId(userId, achievementId))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> service.getProgress(userId, achievementId));
+
+        Mockito.verify(achievementProgressRepository, Mockito.times(1))
+                .findByUserIdAndAchievementId(userId, achievementId);
+        Mockito.verify(achievementProgressMapper, Mockito.never())
+                .toDto(achievementProgress);
+    }
+
+    @Test
+    void giveAchievement_whenOk() {
+        service.giveAchievement(userId, achievement);
+
+        UserAchievement userAchievement = new UserAchievement();
+        userAchievement.setAchievement(achievement);
+        userAchievement.setUserId(userId);
+
+       Mockito.verify(userAchievementRepository, Mockito.times(1))
+               .save(userAchievement);
+    }
+}
