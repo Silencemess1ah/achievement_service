018371523df7fd8d43c57d9019375a06cc75cefc
ifef228 <ifef228@gmail.com>
2024-10-13 08:23:36 +0300

    listener

--- src/main/java/faang/school/achievement/listener/PostEventListener.java
+++ src/main/java/faang/school/achievement/listener/PostEventListener.java
@@ -1,15 +1,33 @@
 package faang.school.achievement.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import faang.school.achievement.dto.PostEvent;
 import faang.school.achievement.handler.EventHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.List;
 
+@Component
+@Slf4j
+@RequiredArgsConstructor
 public class PostEventListener implements MessageListener {
-    private List<EventHandler> handlers;
+    private final List<EventHandler> handlers;
+    private final ObjectMapper objectMapper;
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
-
+        PostEvent postEvent;
+        try {
+            postEvent = objectMapper.readValue(message.getBody(), PostEvent.class);
+        } catch (IOException e) {
+            log.error("cannot get PostEvent from message {}", message, e);
+            throw new RuntimeException(e);
+        }
+        handlers.forEach(handler -> handler.handle(postEvent));
     }
 }
