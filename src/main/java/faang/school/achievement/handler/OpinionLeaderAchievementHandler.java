2e902af9f586378a69aa2175d2b9adb3863f597e
ifef228 <ifef228@gmail.com>
2024-10-13 08:24:26 +0300

    OpinionLeaderAchievementHandler.java

--- src/main/java/faang/school/achievement/handler/OpinionLeaderAchievementHandler.java
+++ src/main/java/faang/school/achievement/handler/OpinionLeaderAchievementHandler.java
@@ -1,12 +1,30 @@
 package faang.school.achievement.handler;
 
+import faang.school.achievement.dto.AchievementProgressDto;
 import faang.school.achievement.dto.PostEvent;
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.service.AchievementService;
+import faang.school.achievement.service.AchievementServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OpinionLeaderAchievementHandler implements EventHandler {
-    @Override
-    public void handle(PostEvent event) {
+    private final AchievementService achievementService;
 
+    @Override
+    @Async
+    public void handle(PostEvent event) {
+        Achievement achievement = new Achievement();//тут надо получить достижение из кеша по названию
+        if (!achievementService.hasAchievement(event.getAuthorId(), achievement.getId())) {
+            achievementService.createProgressIfNecessary(event.getAuthorId(), achievement.getId());
+            AchievementProgressDto achievementProgressDto = achievementService.getProgress(event.getAuthorId(), achievement.getId());
+            achievementProgressDto.setCurrentPoints(achievementProgressDto.getCurrentPoints() + 1);
+            if (achievementProgressDto.getCurrentPoints() == achievement.getPoints()) {
+
+            }
+        }
     }
 }
