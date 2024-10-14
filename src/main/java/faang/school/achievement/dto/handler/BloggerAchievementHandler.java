7ce663247bfff357d6ace95bd3d226b0eb669e8c
Robert Oguzov <robertoguzov2@gmail.com>
2024-10-14 17:55:23 +0300

    Исправил прошлое достежение

--- src/main/java/faang/school/achievement/dto/handler/BloggerAchievementHandler.java
+++ src/main/java/faang/school/achievement/dto/handler/BloggerAchievementHandler.java
@@ -10,54 +10,31 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class BloggerAchievementHandler extends EventHandler<FollowerEvent> {
-
-    private static final String ACHIEVEMENT_NAME = "BLOGGER";
-
-    private final CacheService<Achievement> achievementCacheService;
-    private final AchievementService achievementService;
+public class BloggerAchievementHandler extends AchievementEventHandler<FollowerEvent> {
 
     public BloggerAchievementHandler(CacheService<String> cacheService,
                                      CacheService<Achievement> achievementCacheService,
                                      AchievementService achievementService) {
-        super(cacheService);
-        this.achievementCacheService = achievementCacheService;
-        this.achievementService = achievementService;
+        super(cacheService, achievementCacheService, achievementService);
     }
 
     @Override
-    protected void handleEvent(FollowerEvent event) {
-        log.info("Handling follower event: {}", event);
-        Achievement achievement = achievementCacheService.get(ACHIEVEMENT_NAME, Achievement.class);
-        long userId = event.getUserId(), achievementId = achievement.getId();
-
-        if (!achievementService.hasAchievement(userId, achievementId)) {
-            achievementService.createProgressIfNecessary(userId, achievementId);
-            AchievementProgress achievementProgress = achievementService.getProgress(userId, achievementId);
-            achievementProgress.increment();
-            tryGiveAchievement(achievementProgress, achievement, userId);
-        } else {
-            log.info("User {} has already achievement with id {}", userId, achievementId);
-        }
+    protected String getAchievementName() {
+        return "BLOGGER";
     }
 
     @Override
-    public Class<FollowerEvent> getEventClass() {
+    protected long getUserIdFromEvent(FollowerEvent event) {
+        return event.getUserId();
+    }
+
+    @Override
+    protected Class<FollowerEvent> getEventClass() {
         return FollowerEvent.class;
     }
 
     @Override
     protected Class<?> getHandlerClass() {
         return this.getClass();
-    }
-
-    private void tryGiveAchievement(AchievementProgress achievementProgress, Achievement achievement, long userId) {
-        if (achievementProgress.getCurrentPoints() >= achievement.getPoints()) {
-            log.info("Achievement was successfully issued to the user {}", userId);
-            achievementService.giveAchievement(userId, achievement);
-        } else {
-            log.info("User {} has not yet reached the required number of points to achieve the achievement {}",
-                    userId, achievement.getId());
-        }
     }
 }
