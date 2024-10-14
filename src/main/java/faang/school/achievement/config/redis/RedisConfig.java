536a8cebc0c0b03408bfc635528b128dbd17f782
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-10-14 21:37:09 +0300

    BJS2-26765 redis config new

---
+++ src/main/java/faang/school/achievement/config/redis/RedisConfig.java
@@ -0,0 +1,22 @@
+package faang.school.achievement.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+
+        return container;
+    }
+}
