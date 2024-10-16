f4a5a5ef6656f3f1400da0bad70c2bfed4d8f072
Sergey Sklyar <sklyar1091@gmail.com>
2024-10-16 13:45:49 +0300

    - dto refactor

--- src/main/java/faang/school/achievement/dto/achievement/AchievementProgressDto.java
+++ src/main/java/faang/school/achievement/dto/achievement/AchievementProgressDto.java
@@ -1,10 +1,18 @@
 package faang.school.achievement.dto.achievement;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AchievementProgressDto {
     private long id;
     private long achievementId;
