72e4c16c3e9406b1d06561781932922746c37df6
Sergey Sklyar <sklyar1091@gmail.com>
2024-10-18 00:51:11 +0300

    - refactored cache class - deleted service - refactored tests

--- src/main/java/faang/school/achievement/config/cache/AchievementCache.java
+++ src/main/java/faang/school/achievement/config/cache/AchievementCache.java
@@ -1,16 +1,20 @@
-package faang.school.achievement.cache;
+package faang.school.achievement.config.cache;
 
 import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Component
 @Getter
@@ -19,7 +23,21 @@ import java.util.NoSuchElementException;
 @Validated
 public class AchievementCache {
 
-    private final Map<String, Achievement> cache = new HashMap<>();
+    private final AchievementRepository achievementRepository;
+    private Map<String, Achievement> cache = new ConcurrentHashMap<>();
+
+    @Scheduled(cron = "${schedule.cron-task}")
+    @PostConstruct
+    protected void initCache() {
+        cache = updateCache();
+    }
+
+    public Map<String, Achievement> updateCache() {
+        return achievementRepository.findAll()
+                .stream()
+                .collect(Collectors.toMap(Achievement::getTitle,
+                        achievement -> achievement));
+    }
 
     public Achievement getAchievement(@NotBlank(message = "Title can't be neither blank nor null!")
                                       String achievementTitle) {
---
+++ src/test/java/faang/school/achievement/config/cache/AchievementCacheTest.java
@@ -0,0 +1,74 @@
+package faang.school.achievement.config.cache;
+
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.repository.AchievementRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AchievementCacheTest {
+
+    private static final String VALID_TITLE = "Title 1";
+    private static final String NOT_VALID_TITLE = "123";
+    @InjectMocks
+    private AchievementCache achievementCache;
+    @Mock
+    private AchievementRepository achievementRepository;
+    private Achievement achievement1;
+    private Achievement achievement2;
+
+    @BeforeEach
+    void setUp() {
+        achievement1 = Achievement.builder()
+                .title(VALID_TITLE)
+                .build();
+        achievement2 = Achievement.builder()
+                .title("def")
+                .build();
+    }
+
+    @Test
+    @DisplayName("Init cache of 2 achievements given by DB")
+    public void whenInitMethodCalledThenFillUpCacheFromDB() {
+        when(achievementRepository.findAll()).thenReturn(Arrays.asList(achievement1, achievement2));
+        Map<String, Achievement> cache = achievementCache.updateCache();
+
+        assertEquals(2, cache.size());
+        assertEquals(achievement1, cache.get(achievement1.getTitle()));
+        assertEquals(achievement2, cache.get(achievement2.getTitle()));
+    }
+
+    @Test
+    @DisplayName("When correct title passed return achievement by title")
+    public void whenExistingTitlePassedThenReturnAchievement() {
+        when(achievementRepository.findAll()).thenReturn(Collections.singletonList(achievement1));
+
+        achievementCache.initCache();
+        Achievement result = achievementCache.getAchievement(achievement1.getTitle());
+
+        assertNotNull(result);
+        assertEquals(achievement1.getTitle(), result.getTitle());
+    }
+
+    @Test
+    @DisplayName("When not valid title passed then throw exception")
+    public void whenNonExistingTitlePassedThenThrowException() {
+        assertThrows(NoSuchElementException.class, () ->
+                achievementCache.getAchievement(NOT_VALID_TITLE));
+    }
+}
