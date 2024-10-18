51e189201e42f528339e8b59a949fb6d30a74ad8
Amirhan Almeev <almeevamirhan294@gmail.com>
2024-10-18 21:57:17 +0300

    правки

--- src/main/java/faang/school/achievement/publisher/AchievementPublisher.java
+++ src/main/java/faang/school/achievement/publisher/AchievementPublisher.java
@@ -6,9 +6,7 @@ import faang.school.achievement.dto.AchievementEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
 public class AchievementPublisher {
 
