9b32f61ec2d6ed4df4721493079367b4e9b69c14
Varvara Stepnova <stepnova.varya@yandex.ru>
2024-10-18 05:43:56 +0300

    feature-BJS2-26212: Уведомление о получении достижения fix

--- src/main/java/faang/school/achievement/service/publisher/AchievementEventPublisher.java
+++ src/main/java/faang/school/achievement/service/publisher/AchievementEventPublisher.java
@@ -1,23 +1,28 @@
 package faang.school.achievement.service.publisher;
 
 import faang.school.achievement.dto.event.AchievementEvent;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class AchievementEventPublisher implements MessagePublisher<AchievementEvent> {
 
     private final RedisTemplate redisTemplate;
     private final ChannelTopic channelTopic;
 
+    public AchievementEventPublisher(RedisTemplate redisTemplate,
+                                     @Qualifier("achievementEventTopic") ChannelTopic channelTopic) {
+        this.redisTemplate = redisTemplate;
+        this.channelTopic = channelTopic;
+    }
+
     @Override
     public void publish(AchievementEvent message) {
         redisTemplate.convertAndSend(channelTopic.getTopic(), message);
-        log.info("Message was send {}, in topic - {}", message, channelTopic.getTopic());
+        log.info("AchievementEventPublisher > Message was send {}, in topic - {}", message, channelTopic.getTopic());
     }
 }
