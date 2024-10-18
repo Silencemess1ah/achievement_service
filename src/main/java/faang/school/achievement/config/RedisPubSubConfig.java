c87254d4b653e62d63c24c0edb8085ab31f6a9e9
ifef228 <ifef228@gmail.com>
2024-10-18 19:54:44 +0300

    listener

--- src/main/java/faang/school/achievement/config/RedisPubSubConfig.java
+++ src/main/java/faang/school/achievement/config/RedisPubSubConfig.java
@@ -1,8 +1,33 @@
 package faang.school.achievement.config;
 
+import faang.school.achievement.listener.PostEventListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 @Configuration
 public class RedisPubSubConfig {
+    @Value("${spring.data.redis.channels.post-channel}")
+    private String postTopic;
+    @Bean
+    MessageListenerAdapter postEventListenerAdapter(PostEventListener eventListener) {
+        return new MessageListenerAdapter(eventListener);
+    }
 
+    @Bean
+    ChannelTopic postEventTopic() {
+        return new ChannelTopic(postTopic);
+    }
+
+    @Bean
+    RedisMessageListenerContainer postEventContainer(PostEventListener eventListener, RedisConnectionFactory factory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.addMessageListener(eventListener, postEventTopic());
+        return container;
+    }
 }
--- src/main/java/faang/school/achievement/dto/PostEvent.java
+++ src/main/java/faang/school/achievement/dto/PostEvent.java
@@ -3,10 +3,12 @@ package faang.school.achievement.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PostEvent extends EventBase {
     private long authorId;
     private long postId;
