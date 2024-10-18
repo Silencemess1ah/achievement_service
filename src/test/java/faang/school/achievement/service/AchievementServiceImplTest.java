85c1b169d7261366388aae424374e27f5884cb1b
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-18 17:19:15 +0300

    выполнил

--- src/test/java/faang/school/achievement/service/AchievementServiceImplTest.java
+++ src/test/java/faang/school/achievement/service/AchievementServiceImplTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,7 +30,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(.class)
+@ExtendWith(SpringExtension.class)
 public class AchievementServiceImplTest {
 
     @InjectMocks
