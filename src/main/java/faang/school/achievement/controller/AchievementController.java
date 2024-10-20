5a798b683a44afdba2ee7c06653230af92031762
PLSGOTOASS <koteykaarisha@mail.ru>
2024-10-20 16:27:18 +0300

    поправил

--- src/main/java/faang/school/achievement/controller/AchievementController.java
+++ src/main/java/faang/school/achievement/controller/AchievementController.java
@@ -3,6 +3,7 @@ package faang.school.achievement.controller;
 import faang.school.achievement.dto.AchievementDto;
 import faang.school.achievement.dto.AchievementProgressDto;
 import faang.school.achievement.dto.UserAchievementDto;
+import faang.school.achievement.service.AchievementService;
 import faang.school.achievement.service.AchievementServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,25 +18,25 @@ import java.util.List;
 @RequestMapping("/api/v1/achievements")
 public class AchievementController {
 
-    private final AchievementServiceImpl achievementServiceImpl;
+    private final AchievementService achievementService;
 
     @GetMapping
     public List<AchievementDto> getAchievements() {
-        return achievementServiceImpl.getAllAchievements();
+        return achievementService.getAllAchievements();
     }
 
     @GetMapping("/user/{id}")
     public List<UserAchievementDto> getAchievementsByUserId(@PathVariable Long id) {
-        return achievementServiceImpl.getAllUserAchievements(id);
+        return achievementService.getAllUserAchievements(id);
     }
 
     @GetMapping("/{id}")
     public AchievementDto getAchievementById(@PathVariable Long id) {
-        return achievementServiceImpl.getAchievement(id);
+        return achievementService.getAchievement(id);
     }
 
-    @GetMapping("/user/progress/{id}")
-    public List<AchievementProgressDto> GETUserAchievementProgress(@PathVariable Long id) {
-        return achievementServiceImpl.getUserAchievementProgress(id);
+    @GetMapping("/user/{id}/progress")
+    public List<AchievementProgressDto> GetUserAchievementProgress(@PathVariable Long id) {
+        return achievementService.getUserAchievementProgress(id);
     }
 }
