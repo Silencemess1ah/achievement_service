dc17dcf9580334075ab62aaf90008d6d98997e52
Denis Panasenko <panasenkodenis1995@gmail.com>
2024-10-20 13:11:09 +0300

    BJS2-40397

--- src/main/java/faang/school/achievement/listener/PostEventListenerKafka.java
+++ src/main/java/faang/school/achievement/listener/PostEventListenerKafka.java
@@ -1,14 +1,22 @@
 package faang.school.achievement.listener;
 
+import faang.school.achievement.handler.AbstractEventHandler;
 import faang.school.achievement.model.event.PostEvent;
+import faang.school.achievement.service.AchievementCache;
+import faang.school.achievement.service.AchievementService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostEventListenerKafka {
+public class PostEventListenerKafka extends AbstractEventHandler<PostEvent> {
+    public PostEventListenerKafka(AchievementCache achievementCache,
+                                  AchievementService achievementService,
+                                  String achievementTitle) {
+        super(achievementCache, achievementService, achievementTitle);
+    }
 
     @KafkaListener(topics = "${spring.data.kafka.channel.post_channel}", id = "spring.data.kafka.group")
     public void listen(PostEvent event) {
-        System.out.println(event);
+        super.handle(event);
     }
 }
